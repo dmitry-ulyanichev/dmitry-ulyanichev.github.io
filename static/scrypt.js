8a852c9f66f2831ccfbc841059859460dd91d1d9
dmitry <dmitry.ulyanichev@gmail.com>
2024-06-16 19:25:14 +0200

    first interaction with backend

--- static/scrypt.js
+++ static/scrypt.js
@@ -1,3 +1,25 @@
-window.addEventListener('load', function() {
+document.addEventListener('DOMContentLoaded', function() {
     window.Telegram.WebApp.expand();
-  });
+
+    const userId = 1; // Replace with the actual user ID, possibly retrieved from Telegram Web App context or another source
+
+    fetch(`/api/users/${userId}/referrer-id`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const invitationLink = `https://t.me/HoldemCoinBot?start=${data}`;
+            document.getElementById('invitation-link').textContent = invitationLink;
+        })
+        .catch(error => {
+            console.error('Error fetching referrer ID:', error);
+            document.getElementById('invitation-link').textContent = 'Error loading invitation link';
+        });
+});
+
+function openInvitePage() {
+    window.location.href = 'invite.html';
+}
