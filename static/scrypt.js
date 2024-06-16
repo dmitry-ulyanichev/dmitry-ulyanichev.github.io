0a9157078438f8ef935c88b2188aa88225f70826
dmitry <dmitry.ulyanichev@gmail.com>
2024-06-16 19:50:18 +0200

    try ngrok

--- static/scrypt.js
+++ static/scrypt.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     window.Telegram.WebApp.expand();
 
     const userId = 1; // Replace with the actual user ID, possibly retrieved from Telegram Web App context or another source
+    const apiUrl = 'https://fefe-185-137-140-138.ngrok-free.app';
 
-    fetch(`/api/users/${userId}/referrer-id`)
+    fetch(`${apiUrl}/api/users/${userId}/referrer-id`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
